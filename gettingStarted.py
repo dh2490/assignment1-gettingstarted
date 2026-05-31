dec5bb49cf787709fc9ad354f1879a7663f56b2a
domh <dh2490@nyu.edu>
2026-05-30 21:55:48 -0400

    Assignment fixed

--- gettingStarted.py
+++ gettingStarted.py
@@ -20,7 +20,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = int("1")
+        answer = int("4")
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = int("3")
     else: 
@@ -34,7 +34,7 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = ("What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number")
+    debug_question = "What is the SHA256 hashing value of your NYU email and use the answer in your code - "
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
